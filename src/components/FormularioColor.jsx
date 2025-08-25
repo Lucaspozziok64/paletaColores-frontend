71c69af8382eacfae70e4d8c796f4232b45629f9
Lucasfi <lukafigueroa64@gmail.com>
2025-08-25 13:13:14 -0300

    logramos capturar el nombre color hexadecimal

--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 
 const FormularioColor = () => {
+  const [colorInput, setColorInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(colorInput)
+  };
   return (
-    <form className="d-flex justify-content-center align-items-center">
+    <form onSubmit={handleSubmit} className="d-flex justify-content-center align-items-center">
       <div className="w-100 text-center">
         <h2 className="w-100">🎨Paleta de colores🎨</h2>
         <div className="d-flex justify-content-center align-items-center mb-1">
           <label className="form-label">
             <strong>Selecciona un Color:</strong>
           </label>
-          <input type="color" className="mx-3 mb-1" />
+          <input
+            type="color"
+            className="mx-3 mb-1"
+            onChange={(e) => setColorInput(e.target.value)}
+            value={colorInput}
+          />
           <button type="submit" className="btn btn-success botonAgregar">
             <i className="bi bi-plus-circle mx-1 text-white"></i>Agregar
           </button>
