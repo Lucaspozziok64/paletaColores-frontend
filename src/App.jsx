950847b6f9e9fab7319032ab0ac58710be0a7e87
Lucasfi <lukafigueroa64@gmail.com>
2025-08-25 11:30:08 -0300

    agregue gradiente de fondo y modifique color del formulario y color del titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import FormularioColor from "./components/FormularioColor"
-import 'bootstrap/dist/css/bootstrap.min.css'
+import FormularioColor from "./components/FormularioColor";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
   return (
     <>
-      <h1 className="text-center">Bienvenido al frontend de paleta de Colores</h1>
-      <section className="container my-5 bg-primary">
-        <FormularioColor />
-      </section>
+      <main>
+        <h1 className="text-center text-light my-5">
+          Bienvenido al frontend de paleta de Colores
+        </h1>
+        <section className="container my-5 bg-body-tertiary rounded-3">
+          <FormularioColor />
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -3,10 +3,10 @@ const FormularioColor = () => {
   return (
     <form className="d-flex justify-content-center align-items-center">
       <div className="w-100 text-center">
-        <h2 className="w-100">Paleta de colores🎨</h2>
+        <h2 className="w-100">🎨Paleta de colores🎨</h2>
         <div className="d-flex justify-content-center align-items-center mb-1">
           <label className="form-label">
-            <strong className="text-light">Selecciona un Color:</strong>
+            <strong>Selecciona un Color:</strong>
           </label>
           <input type="color" className="mx-3 mb-1" />
           <button type="submit" className="btn btn-success botonAgregar">
