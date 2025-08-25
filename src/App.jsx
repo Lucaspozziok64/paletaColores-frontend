7a290102e4e33848eb1df0ff2ce2a8a0729b36ba
Lucasfi <lukafigueroa64@gmail.com>
2025-08-25 15:30:57 -0300

    Agregue mensaje al usuario con sweetAlert2

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,50 @@ import ListaColores from "./components/ListaColores";
 import { Row } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import Swal from "sweetalert2";
 
 function App() {
-  const colorLocalStorage = JSON.parse(localStorage.getItem("listaColores")) || [];
+  const colorLocalStorage =
+    JSON.parse(localStorage.getItem("listaColores")) || [];
   const [colores, setColores] = useState(colorLocalStorage);
 
-  const agregarColor = (nuevoColor)=> {
-    const nuevo = {id: uuidv4(), nombreColor: nuevoColor}
-    setColores([...colores, nuevo])
-  }
+  const agregarColor = (nuevoColor) => {
+    const nuevo = { id: uuidv4(), nombreColor: nuevoColor };
+    setColores([...colores, nuevo]);
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Color agregado exitosamente",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
 
-  const borrarColor = (id)=> {
-    setColores(colores.filter(c => c.id !== id));
-    console.log(id)
-  }
+  const borrarColor = (id) => {
+    Swal.fire({
+      title: "Estas seguro/a de eliminar este color?",
+      text: "No podras revertir esto",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, eliminar!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        setColores(colores.filter((c) => c.id !== id));
+        console.log(id);
+        Swal.fire({
+          title: "Eliminado!",
+          text: "El color ha sido eliminado",
+          icon: "success",
+        });
+      }
+    });
+  };
 
-  useEffect(()=> {
-    localStorage.setItem("listaColores", JSON.stringify(colores))
-  }, [colores])
+  useEffect(() => {
+    localStorage.setItem("listaColores", JSON.stringify(colores));
+  }, [colores]);
 
   return (
     <>
@@ -34,11 +60,13 @@ function App() {
         </section>
       </main>
       <Row className="container-fluid row-gap-3">
-        {
-          colores.map((color)=> 
-            <ListaColores key={color.id} color={color} borrarColor={()=> borrarColor(color.id)} />
-          )
-        }
+        {colores.map((color) => (
+          <ListaColores
+            key={color.id}
+            color={color}
+            borrarColor={() => borrarColor(color.id)}
+          />
+        ))}
       </Row>
     </>
   );
